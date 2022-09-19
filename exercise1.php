900d732e5ae06e265973ac1c27806f44461fdd2d
JamesDampulas <113904006+JamesDampulas@users.noreply.github.com>
2022-09-19 19:26:44 +0800

    Add files via upload

---
+++ exercise1.php
@@ -0,0 +1,15 @@
+<?php
+
+echo "Twinkle, Twinkle little star\r\n";
+echo "<br>";
+
+$twi = "Twinkle";
+$str = "star";
+echo "$twi, $twi little $str \r\n";
+echo "<br>";
+
+$twi = "Cursed";
+$str = "world";
+echo "\r\n$twi, $twi little $str";
+
+?>
---
+++ exercise4.php
@@ -0,0 +1,16 @@
+<?php
+
+$Name = 'Harry';
+$Age = 28;
+
+var_dump($Name);
+echo "<br>";
+print_r($Name);
+echo "<br>";
+echo "\n";
+var_dump($Age);
+echo "<br>";
+$Name=null;
+var_dump($Name);
+echo "<br>";
+?>
